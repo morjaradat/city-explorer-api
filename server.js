ee5478bf100b84d22454a18dbb7e3ba3f8b1052a
Mohammad jaradat <morjaradat66@gmail.com>
2021-05-04 15:43:51 +0300

    add weather-data

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const weather = require('./assets/weather.json')
+const cors =require('cors')
+// require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/weather', function (req, res) {
+    const arrOfData=weather.data.map(data=>new Weather(data))
+    res.send(arrOfData);
+  })
+
+class Weather{
+    constructor(data){
+        this.date=data.valid_date;
+        this.description =data.weather.description;
+    }
+}
+
+app.listen(3099)
