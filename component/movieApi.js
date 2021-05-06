ac9361cba7179996a7397eea0520aecfa8afbc15
Mohammad jaradat <morjaradat66@gmail.com>
2021-05-07 02:48:47 +0300

    added cache

--- component/movieApi.js
+++ component/movieApi.js
@@ -3,19 +3,26 @@ require('dotenv').config()
 
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY
 const MOVIE_API_URL = process.env.MOVIE_API_URL
-
+const cache ={};
 const movieApi = (req, res) => {
-    try {
-        const movieUrl = `${MOVIE_API_URL}?api_key=${MOVIE_API_KEY}&language=en-US&include_adult=false&query=${req.query.query}`
-        superagent.get(movieUrl).then(movieData => {
-            console.log(movieData.body.results)
-            const arrOfData = movieData.body.results.map(mdata => new Movie(mdata))
-            console.log(arrOfData)
-            res.send(arrOfData);
-        })
-    } catch (error) {
-        console.log('erorr')
-        res.send(error);
+    if (cache[req.query.query] !== undefined) {
+        console.log('from IF ');
+        res.send(cache[req.query.query]);
+    } else {
+        console.log('ELSE ');
+        try {
+            const movieUrl = `${MOVIE_API_URL}?api_key=${MOVIE_API_KEY}&language=en-US&include_adult=false&query=${req.query.query}`
+            superagent.get(movieUrl).then(movieData => {
+                // console.log(movieData.body.results)
+                const arrOfData = movieData.body.results.map(mdata => new Movie(mdata))
+                // console.log(arrOfData)
+                cache[req.query.query] =arrOfData ;
+                res.send(arrOfData);
+            })
+        } catch (error) {
+            // console.log('erorr')
+            res.send(error);
+        }
     }
 
 }
--- component/weatherApi.js
+++ component/weatherApi.js
@@ -4,20 +4,35 @@ require('dotenv').config()
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 const WEATHER_API_URL=process.env.WEATHER_API_URL
 
-const weatherApi = (req, res)=> {
-  try {
-    const weatherBitUrl = `${WEATHER_API_URL}?key=${WEATHER_API_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`
+const cache  ={};
 
-    superagent.get(weatherBitUrl).then(weatherBitData => {
-      // console.log(weatherBitData.body.data)
-      const arrOfData = weatherBitData.body.data.map(data => new Weather(data))
+const weatherApi = (req, res)=> {
+  // const lat = req.query.lat
+  // const lon = req.query.lon
+  if (cache[(req.query.lat,req.query.lon)] !== undefined) {
+    // console.log(`this is stucture  ${cache[(req.query.lat,req.query.lon)]}`);
+    // console.log(`this is cache ${cache}`);
+    // console.log('from IF ');
+    res.send(cache[(req.query.lat,req.query.lon)])
+
+  } else {
+    console.log('ELSE ');
+    try {
+      const weatherBitUrl = `${WEATHER_API_URL}?key=${WEATHER_API_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`
+  
+      superagent.get(weatherBitUrl).then(weatherBitData => {
+        // console.log(weatherBitData.body.data)
+        const arrOfData = weatherBitData.body.data.map(data => new Weather(data))
+        cache[(req.query.lat,req.query.lon)] = arrOfData;
+        res.send(arrOfData);
+      })
+    } catch (error) {
+      // console.log('erorr')
+      const arrOfData = weather.data.map(data => new Weather(data))
       res.send(arrOfData);
-    })
-  } catch (error) {
-    // console.log('erorr')
-    const arrOfData = weather.data.map(data => new Weather(data))
-    res.send(arrOfData);
+    }
   }
+  
 
 }
 
